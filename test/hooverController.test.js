61af7ed8c74158a73eee5d9a99bc50f2c06b43a2
Alex Burt <MrBurtyyy@hotmail.com>
2019-06-10 23:18:46 +0100

    Add more units tests for hooverController functions

--- test/hooverController.test.js
+++ test/hooverController.test.js
@@ -42,4 +42,58 @@ describe("Test the hoover controller", () => {
     expect(validPosition).toEqual([roomX, roomY]);
     done();
   });
+
+  test("getIndexOfMatchingDirtPatch - hoover on dirt patch (> -1)", done => {
+    const hooverX = 13;
+    const hooverY = 18;
+    const patches = [[1, 3], [12, 25], [13, 18]];
+    const patchIndex = hooverController.getIndexOfMatchingDirtPatch(
+      hooverX,
+      hooverY,
+      patches
+    );
+    expect(patchIndex).toBeGreaterThan(-1);
+    done();
+  });
+
+  test("getIndexOfMatchingDirtPatch - hoover not on dirt patch (== -1)", done => {
+    const hooverX = 13;
+    const hooverY = 18;
+    const patches = [[1, 3], [12, 25]];
+    const patchIndex = hooverController.getIndexOfMatchingDirtPatch(
+      hooverX,
+      hooverY,
+      patches
+    );
+    expect(patchIndex).toEqual(-1);
+    done();
+  });
+
+  test("checkAndRemoveDirtPatch - hoover on a dirt patch (item is removed)", done => {
+    const hooverX = 13;
+    const hooverY = 18;
+    const patches = [[1, 3], [12, 25], [13, 18]];
+    const expectedPatches = [[1, 3], [12, 25]];
+    const newPatches = hooverController.checkAndRemoveDirtPatch(
+      hooverX,
+      hooverY,
+      patches
+    );
+    expect(newPatches).toEqual(expectedPatches);
+    done();
+  });
+
+  test("checkAndRemoveDirtPatch - hoover not a dirt patch (nothing is removed)", done => {
+    const hooverX = 13;
+    const hooverY = 12;
+    const patches = [[1, 3], [12, 25], [13, 18]];
+    const expectedPatches = [[1, 3], [12, 25], [13, 18]];
+    const newPatches = hooverController.checkAndRemoveDirtPatch(
+      hooverX,
+      hooverY,
+      patches
+    );
+    expect(newPatches).toEqual(expectedPatches);
+    done();
+  });
 });
